1611486d442c3bb2d87da6e87b315bd54943fb19
takecare <rui@vazteixeira.org>
2018-08-05 23:24:40 +0100

    section 4 (before assignment)

---
+++ section4/src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Person.css'
+
+const person = (props) => {
+  const age = props.person.age || Math.floor(Math.random() * 30)
+  const children = props.children
+
+  return (
+    <div className='Person' onClick={props.click}>
+      <p>I'm a person, my name is {props.person.name} and I am {age} years old.</p>
+      <p>{children}</p>
+      <input type="text" onChange={props.nameChanged} value={props.person.name} />
+    </div>
+  )
+}
+
+export default person;
