3f977a24dae2506bdf41d3268f0f7d37c0b0b5e2
takecare <rui@vazteixeira.org>
2019-01-17 20:19:21 +0000

    extract cockpit toggle button style

--- section7/src/components/Cockpit/Cockpit.js
+++ section7/src/components/Cockpit/Cockpit.js
@@ -2,17 +2,17 @@ import React from 'react'
 import styles from './Cockpit.css';
 
 export const Cockpit = function (props) {
-  const styleClasses = props.totalNumberOfPersons <= 1 ? [styles.red, styles.bold].join(' ') : [styles.red]
-  const toggleStyle = props.showingPersons ? styles.Red : ['']
+  const textStyle = props.totalNumberOfPersons <= 1 ? [styles.red, styles.bold].join(' ') : [styles.red]
+  const toggleButtonStyle = [styles.CockpitButton, props.showingPersons ? styles.Red : ['']].join(' ')
   return (
     <div className={styles.Cockpit}>
       <header>
         {React.createElement('h1', { className: 'title' }, props.title)}
-        <p className={styleClasses}>It is working!</p>
+        <p className={textStyle}>It is working!</p>
       </header>
 
       <button onClick={() => props.ageChangeHandler(10)}>Set all ages to 10</button>
-      <button className={toggleStyle} onClick={props.togglePersonsHandler}>Toggle Persons</button>
+      <button className={toggleButtonStyle} onClick={props.togglePersonsHandler}>Toggle Persons</button>
     </div>
   )
 }
